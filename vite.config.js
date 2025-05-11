763bc2d03c8285134da9e4c6ecf8e75dfa60ceed
Jean1000 <157409715+Jean1000levrai@users.noreply.github.com>
2025-05-11 14:53:44 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-exoprt default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH ||  "/website-for-habittracker",
-}];
